ea03c36289936b25514e57e0eaedd4e6ba528322
homeless <vlad.sarkulov@gmail.com>
2023-09-07 22:43:51 +0300

    first step

--- Swiftme-FirstApp./main.swift
+++ Swiftme-FirstApp./main.swift
@@ -5,7 +5,9 @@
 //  Created by @hmlssgd on 07.09.2023.
 //
 
-import Foundation
-
-print("Hello, World!")
-
+print("Введите первое значение")
+// получение первого значения
+var a = readLine()
+print("Введите второе значение")
+// получение второго значения
+var b = readLine()
